686f6dc37b01776c96ff08a7d48f5fb6e0a04424
dingxc <775862230@qq.com>
2018-04-19 18:04:25 +0800

    迁移数据库，只放迁移 代码

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -13,17 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
-        \App\Console\Commands\Upload::class,
-        \App\Console\Commands\TestCommand::class,
-        \App\Console\Commands\Vip::class,
-        \App\Console\Commands\DayOrder::class,
-        \App\Console\Commands\BrandSale::class,
-        \App\Console\Commands\SendDingDing::class,
-        \App\Console\Commands\Convert::class,
-        \App\Console\Commands\SendBrandSale::class,
-        \App\Console\Commands\ZhenYL::class,
-        \App\Console\Commands\SendZhenYL::class,
     ];
 
     /**
@@ -34,15 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:convert')->dailyAt("08:35");   //生成图片(会员体系)
-        $schedule->command('command:sendDingDing')->dailyAt("09:10");   //发送图片(会员体系群)
-        $schedule->command('command:dayOrder')->dailyAt("09:11");   //发送图片(店长群)
 
-        $schedule->command('command:brandSale')->dailyAt("22:55");   //生成图片(销售占比图片)
-        $schedule->command('command:sendBrandSale')->dailyAt("23:00");   //发送图片(销售额占比，发销售额占比群)
-
-        $schedule->command('command:zhenYL')->cron("1 12,18 * * *");    //郑伊露需要的报表
-        $schedule->command('command:zhenYL')->dailyAt('22:30');    //郑伊露需要的报表
 
     }
 
